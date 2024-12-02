bb9c112b88a337a972501fa1b289bb375b928494
simtaejin <stj53@nate.com>
2024-12-03 00:23:43 +0900

    myfavorites 수정

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,6 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'custom_auth' => \App\Http\Middleware\CustomAuth::class,
-        'myfavorites' => \App\Http\Middleware\Myfavorites::class,
+        'myfavorites' => \App\Http\Middleware\MyFavorites::class,
     ];
 }
