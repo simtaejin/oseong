d4225ae915edf977995233801f6964734ac03aba
simtaejin <stj53@nate.com>
2024-12-10 00:15:42 +0900

    러쉬 5 배열표 쿠팡 파트너스

--- resources/views/arraytable/ruch5.blade.php
+++ resources/views/arraytable/ruch5.blade.php
@@ -85,5 +85,10 @@
 
 
         </div>
+
+        <script>
+            new PartnersCoupang.G({"id":823680,"template":"carousel","trackingCode":"AF6935902","width":"100%","height":"140","tsource":""});
+        </script>
+        <span style="font-size: xx-small">"이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의 수수료를 제공받습니다."</span>
     </main>
 @endsection
